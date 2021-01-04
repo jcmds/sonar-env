57a681bfab45f767791056c5c3e10bca8ca2b9ec
jean.carlos@5by5.com.br <jean.carlos>
2021-01-04 19:30:43 -0300

    Create shell script from volume configuration in linux

---
+++ volume-configure.sh
@@ -0,0 +1,4 @@
+    mkdir -p /var/sonarqube/config && chown -R 1000:1000  /var/sonarqube/config && \
+    mkdir -p /var/sonarqube/data  && chown -R 1000:1000  /var/sonarqube/data && \
+    mkdir -p /var/sonarqube/log  && chown -R 1000:1000  /var/sonarqube/log && \
+    mkdir -p /var/sonarqube/extension  && chown -R 1000:1000  /var/sonarqube/extension
