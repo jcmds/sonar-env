b1101f90bd0c931102c5086d4c6ef5cd48efbfd1
jean.carlos@5by5.com.br <jean.carlos>
2021-01-04 19:36:26 -0300

    Added chmod command

--- volume-configure.sh
+++ volume-configure.sh
@@ -1,4 +1,4 @@
-    mkdir -p /var/sonarqube/config && chown -R 1000:1000  /var/sonarqube/config && \
-    mkdir -p /var/sonarqube/data  && chown -R 1000:1000  /var/sonarqube/data && \
-    mkdir -p /var/sonarqube/log  && chown -R 1000:1000  /var/sonarqube/log && \
-    mkdir -p /var/sonarqube/extension  && chown -R 1000:1000  /var/sonarqube/extension
+    mkdir -p /var/sonarqube/config && \
+    mkdir -p /var/sonarqube/data  && \
+    mkdir -p /var/sonarqube/log   \
+    mkdir -p /var/sonarqube/extension  && chmod 777 -R /var/sonarqube/
