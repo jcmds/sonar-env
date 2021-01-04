996451de9a2049c31764520e26b25a3cc1405b03
jean.carlos@5by5.com.br <jean.carlos>
2021-01-04 19:36:50 -0300

    Fixed script

--- volume-configure.sh
+++ volume-configure.sh
@@ -1,4 +1,4 @@
     mkdir -p /var/sonarqube/config && \
     mkdir -p /var/sonarqube/data  && \
-    mkdir -p /var/sonarqube/log   \
+    mkdir -p /var/sonarqube/log  && \
     mkdir -p /var/sonarqube/extension  && chmod 777 -R /var/sonarqube/
